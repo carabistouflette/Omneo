d191fdc77a066f4ff22a6fc8ef3fec60df2fa1fa
arobin <arobin@smag.tech>
2025-01-28 10:55:53 +0100

    feat(Cryptography:encrypt): AbstractCipher done

--- src/main/kotlin/cryptography/encryption/AbstractCipher.kt
+++ src/main/kotlin/cryptography/encryption/AbstractCipher.kt
@@ -1,18 +1,26 @@
 package main.kotlin.cryptography.encryption
 
-
 abstract class AbstractCipher : Cipher {
 
     override fun encrypt(data: ByteArray?): ByteArray? {
-        // Implémentation de base ou logique commune
-        return performEncryption(data)
+        if (data == null) return null
+
+        val processedData = preprocessData(data)
+        return performEncryption(processedData)
     }
 
     override fun decrypt(encryptedData: ByteArray?): ByteArray? {
-        // Implémentation de base ou logique commune
-        return performDecryption(encryptedData)
+        if (encryptedData == null) return null
+
+        val processedData = preprocessData(encryptedData)
+        return performDecryption(processedData)
     }
 
-    protected abstract fun performEncryption(data: ByteArray?): ByteArray?
-    protected abstract fun performDecryption(encryptedData: ByteArray?): ByteArray?
+    private fun preprocessData(data: ByteArray): ByteArray {
+        // If preprocess is needed anytime
+        return data
+    }
+
+    protected abstract fun performEncryption(data: ByteArray): ByteArray?
+    protected abstract fun performDecryption(encryptedData: ByteArray): ByteArray?
 }
