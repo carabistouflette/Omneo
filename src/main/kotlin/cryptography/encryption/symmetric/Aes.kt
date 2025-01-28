327d0f92063a67569720949c17b03ae2de21aa49
arobin <arobin@smag.tech>
2025-01-28 10:57:00 +0100

    feat(Cryptography:encrypt:symmetric): Aes done

--- src/main/kotlin/cryptography/encryption/symmetric/Aes.kt
+++ src/main/kotlin/cryptography/encryption/symmetric/Aes.kt
@@ -1,15 +1,21 @@
 package main.kotlin.cryptography.encryption.symmetric
 
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
 
 class Aes : AbstractSymmetricCipher() {
 
-    override fun performEncryption(data: ByteArray?): ByteArray? {
-        // Implémentation de l'encryption AES
-        return data // Placeholder
+    private val secretKeySpec = SecretKeySpec(key, "AES")
+
+    override fun performEncryption(data: ByteArray): ByteArray? {
+        val cipher = Cipher.getInstance("AES")
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec)
+        return cipher.doFinal(data)
     }
 
-    override fun performDecryption(encryptedData: ByteArray?): ByteArray? {
-        // Implémentation de la décryption AES
-        return encryptedData // Placeholder
+    override fun performDecryption(encryptedData: ByteArray): ByteArray? {
+        val cipher = Cipher.getInstance("AES")
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec)
+        return cipher.doFinal(encryptedData)
     }
 }
